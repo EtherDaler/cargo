4040b5009007ade54639f8be339dbc79c62c7eb7
EtherDaler <63519670+EtherDaler@users.noreply.github.com>
2023-05-04 10:24:46 +0300

    add migrations2

--- mainApp/migrations/0001_initial.py
+++ mainApp/migrations/0001_initial.py
@@ -4,8 +4,9 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
-class Migration(migrations.Migration):
+#
 
+class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
