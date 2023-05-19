08c46858e0efd92a94430b9ecf680a4e723c8204
EtherDaler <63519670+EtherDaler@users.noreply.github.com>
2023-05-19 23:24:23 +0300

    fix build bugs2

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
   // eslint-disable-next-line no-undef
   outputDir: path.resolve(__dirname, '../static/src/vue/dist/'), // Output to a directory in STATICFILES_DIRS
   indexPath: '../../../../templates/start/index.html',
-  filenameHashing: false, // Django will hash file names, not webpack
+  filenameHashing: true, // Django will hash file names, not webpack
   runtimeCompiler: true, // See: https://vuejs.org/v2/guide/installation.html#Runtime-Compiler-vs-Runtime-only
 
   transpileDependencies: [
